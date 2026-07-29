3010329cc789ef7404b1903d57b861c7b4397531
Guilherme de Castro <guilherme.castro@totvs.com.br>
2026-07-29 01:00:04 -0300

    Inject deployment version into Charts site binary (#7)
    * feat(site): inject deployed Charts version
    
    * fix(ci): validate release tags as strict SemVer

--- site/cmd/server/main.go
+++ site/cmd/server/main.go
@@ -6,13 +6,19 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/araihu/goshtoso-charts/site/internal/buildinfo"
 	"github.com/araihu/goshtoso-charts/site/internal/server"
 )
 
 func main() {
 	port := flag.Int("port", 8091, "HTTP port")
+	showVersion := flag.Bool("version", false, "print site version and exit")
 	flag.Parse()
+	if *showVersion {
+		fmt.Println(buildinfo.SiteVersion())
+		return
+	}
 	address := fmt.Sprintf(":%d", *port)
-	log.Printf("Goshtoso Charts demo: http://localhost%s", address)
+	log.Printf("Goshtoso Charts demo %s: http://localhost%s", buildinfo.SiteVersion(), address)
 	log.Fatal(http.ListenAndServe(address, server.New()))
 }
