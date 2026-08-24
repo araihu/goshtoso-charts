57921ffb00f5b279875d743fea9f1e4e3020509c
Guilherme de Castro <guilherme.castro@totvs.com.br>
2026-08-24 17:12:12 -0300

    build: upgrade to Go 1.27.0

--- .dagger/src/index.ts
+++ .dagger/src/index.ts
@@ -10,7 +10,7 @@ import {
 } from "@dagger.io/dagger"
 
 const GO_IMAGE =
-  "golang:1.26.5-bookworm@sha256:53eeac89074db483fdf0ab3be1df32bf6e47562263d2d0d6baa7f26acb4957dd"
+  "golang:1.27.0-bookworm@sha256:484ef6066fa69acb059fdfeda7ba2b8f7391f2ef6abc6f9b8411e669ebd56466"
 const JQ_IMAGE =
   "ghcr.io/jqlang/jq:1.8.2@sha256:b9c68867e5766576263a222e91db3de422d802069c7af70440e667a95344e486"
 
